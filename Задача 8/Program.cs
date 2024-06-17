af6dc460b954352fc3eb3c7a33902f93cfa14fc2
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-18 01:04:21 +0300

    Add files via upload

---
+++ Задача 8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем объект класса User с указанием данных о пользователе и даты регистрации
+            DateTime registrationDate = DateTime.Now.Date; // Пример: текущая дата
+            User user = new User("user123", "Иван", "Иванов", 30, registrationDate);
+
+            // Выводим информацию о пользователе на экран
+            user.PrintUserInfo();
+
+            // Ожидаем нажатия клавиши перед завершением программы
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Задача 8/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_8
+{
+    class User
+    {
+
+        // Поля класса User
+        private string login;          // Логин пользователя
+        private string firstName;      // Имя пользователя
+        private string lastName;       // Фамилия пользователя
+        private int age;               // Возраст пользователя
+        private readonly DateTime registrationDate;  // Дата заполнения анкеты
+
+        // Конструктор класса User для инициализации полей
+        public User(string login, string firstName, string lastName, int age, DateTime registrationDate)
+        {
+            this.login = login;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.registrationDate = registrationDate;
+        }
+
+        // Метод для вывода информации о пользователе на экран
+        public void PrintUserInfo()
+        {
+            Console.WriteLine("Информация о пользователе:");
+            Console.WriteLine($"Логин: {login}");
+            Console.WriteLine($"Имя: {firstName}");
+            Console.WriteLine($"Фамилия: {lastName}");
+            Console.WriteLine($"Возраст: {age}");
+            Console.WriteLine($"Дата регистрации: {registrationDate.ToShortDateString()}");
+        }
+
+    }
+}
