b1def8af692a103293a616c09767ecabffbf475e
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-17 21:29:22 +0300

    Add files via upload

---
+++ Задача 4/Задача 4/Addres.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_4
+{
+    public class Addres
+    {
+        private string index;
+        private string country;
+        private string city;
+        private string street;
+        private string house;
+        private string apartment;
+
+        public string Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+
+        public string House
+        {
+            get { return house; }
+            set { house = value; }
+        }
+
+        public string Apartment
+        {
+            get { return apartment; }
+            set { apartment = value; }
+        }
+
+
+        //2-ой вариант решения
+
+        //public string Index { get; set; }
+        //public string Country { get; set; }
+        //public string City { get; set; }
+        //public string Street { get; set; }
+        //public string House { get; set; }
+        //public string Apartment { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return $"Индекс: {Index}, Страна: {Country}, Город: {City}, Улица: {Street}, Дом: {House}, Квартира: {Apartment}";
+        //}
+
+    }
+}
---
+++ Задача 4/Задача 4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем экземпляр класса Address
+            Addres myAddress = new Addres();
+
+            // Заполняем поля экземпляра
+            myAddress.Index = "123456";
+            myAddress.Country = "Россия";
+            myAddress.City = "Москва";
+            myAddress.Street = "Тверская";
+            myAddress.House = "1";
+            myAddress.Apartment = "10";
+
+            // Выводим на экран значения полей
+            Console.WriteLine("Почтовый адрес:");
+            Console.WriteLine("Индекс: " + myAddress.Index);
+            Console.WriteLine("Страна: " + myAddress.Country);
+            Console.WriteLine("Улица: " + myAddress.City);
+            Console.WriteLine("Дом: " + myAddress.House);
+            Console.WriteLine("Квартира: " + myAddress.Apartment);
+
+
+            //2-ой вариант решения
+
+            //Address myAddress = new Address
+            //{
+            //    Index = "123456",
+            //    Country = "Россия",
+            //    City = "Москва",
+            //    Street = "Тверская",
+            //    House = "1",
+            //    Apartment = "10"
+            //};
+
+            //Console.WriteLine("Почтовый адрес:");
+            //Console.WriteLine(myAddress.ToString());
+
+            Console.ReadKey();
+        }
+       
+    }
+}
