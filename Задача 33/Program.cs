5de0dad17058a677ab35bc9a756be8580a5ca170
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-19 14:32:22 +0300

    Add files via upload

---
+++ Задача 33/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.Method();
+            Console.ReadLine();
+        }
+    }
+}
