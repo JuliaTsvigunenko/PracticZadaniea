571e54b385df9605d79bdcb226d95e8887203820
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-19 14:53:12 +0300

    Add files via upload

---
+++ Задача 41/Point.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_41
+{
+    // Обобщенный класс Point
+    public class Point<T>
+    {
+        // Внутренние поля координат точки
+        private T x;
+        private T y;
+
+        // Конструктор с двумя параметрами для инициализации координат
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Свойства доступа к внутренним полям класса
+        public T X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public T Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        // Метод для вывода значений внутренних полей класса
+        public void Print()
+        {
+            Console.WriteLine($"Координаты точки: ({x}, {y})");
+        }
+    }
+}
---
+++ Задача 41/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание экземпляров Point с разными типами данных
+            Point<int> intPoint = new Point<int>(3, 5);
+            Point<uint> uintPoint = new Point<uint>(10, 20);
+            Point<double> doublePoint = new Point<double>(2.5, 4.8);
+
+            // Вывод координат точек
+            Console.WriteLine("Целочисленная точка:");
+            intPoint.Print();
+
+            Console.WriteLine("\nЦелочисленная точка без знака:");
+            uintPoint.Print();
+
+            Console.WriteLine("\nДробная точка:");
+            doublePoint.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
