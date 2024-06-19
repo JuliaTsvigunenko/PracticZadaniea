a68c8fedd9965b988e05b6a18ca62661f9db1371
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-19 15:52:44 +0300

    Add files via upload

---
+++ Задача 49/Point.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_49
+{
+    class Point
+    {
+        private int x;
+        private int y;
+
+        // Статические поля для подсчета точек в каждой координатной четверти
+        private static int countQ1;
+        private static int countQ2;
+        private static int countQ3;
+        private static int countQ4;
+        private static int totalCount;
+
+        // Конструктор с параметрами для инициализации координат точки
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            UpdateQuadrantCounts();
+            totalCount++;
+        }
+
+        // Метод для вывода координат точки
+        public void PrintCoordinates()
+        {
+            Console.WriteLine($"Точка с координатами ({x}, {y})");
+        }
+
+        // Статический метод для вывода количества точек в каждой координатной четверти и общего количества точек
+        public static void PrintPointCounts()
+        {
+            Console.WriteLine("Количество точек:");
+            Console.WriteLine($"1-я четверть: {countQ1}");
+            Console.WriteLine($"2-я четверть: {countQ2}");
+            Console.WriteLine($"3-я четверть: {countQ3}");
+            Console.WriteLine($"4-я четверть: {countQ4}");
+            Console.WriteLine($"Всего точек: {totalCount}");
+        }
+
+        // Приватный метод для обновления счетчиков точек в четвертях
+        private void UpdateQuadrantCounts()
+        {
+            if (x > 0 && y > 0)
+            {
+                countQ1++;
+            }
+            else if (x < 0 && y > 0)
+            {
+                countQ2++;
+            }
+            else if (x < 0 && y < 0)
+            {
+                countQ3++;
+            }
+            else if (x > 0 && y < 0)
+            {
+                countQ4++;
+            }
+            // Если точка лежит на осях координат, не учитываем ее в четвертях
+        }
+    }
+}
---
+++ Задача 49/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Создание нескольких точек
+            Point point1 = new Point(2, 3);
+            Point point2 = new Point(-1, 5);
+            Point point3 = new Point(-3, -4);
+
+            // Вывод координат каждой точки
+            point1.PrintCoordinates();
+            point2.PrintCoordinates();
+            point3.PrintCoordinates();
+
+            // Вывод количества точек в каждой четверти и общего количества точек
+            Point.PrintPointCounts();
+
+            Console.ReadLine();
+        }
+    }
+
+    //Объяснение:
+    //Класс Point: Описывает точку на плоскости с координатами x и y.
+    //Статические поля: countQ1, countQ2, countQ3, countQ4, totalCount используются для подсчета точек в каждой четверти и общего количества точек.
+    //Конструктор Point(int x, int y): Инициализирует координаты точки и обновляет счетчики точек в соответствующих четвертях.
+    //Метод PrintCoordinates(): Выводит на экран координаты текущей точки.
+    //Статический метод PrintPointCounts(): Выводит на экран количество точек в каждой четверти и общее количество точек.
+    //Программа в методе Main() создает несколько точек, выводит их координаты и затем выводит общее количество точек и количество точек в каждой четверти с помощью статического метода PrintPointCounts() класса Point.
+
+
+
+}
