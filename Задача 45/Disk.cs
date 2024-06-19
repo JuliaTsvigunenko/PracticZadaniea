ebd1ef450ed81d25a04ced1cb895481175dbdc7f
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-19 15:16:21 +0300

    Add files via upload

---
+++ Задача 45/Disk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_45
+{
+    class Disk : IShape
+    {
+        public double Radius { get; }
+
+        public Disk(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override string ToString()
+        {
+            return $"Disk (Radius: {Radius}, Perimeter: {Perimeter():f2}, Area: {Area():f2})";
+        }
+    }
+}
