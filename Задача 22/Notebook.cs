0f08608f68dedf44e9a9d8a8f94c16fd7f3e55c3
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-19 16:16:12 +0300

    Add files via upload

---
+++ Задача 22/Notebook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_22
+{
+    struct Notebook
+    {
+        public string Model { get; private set; }
+        public string Manufacturer { get; private set; }
+        public decimal Price { get; private set; }
+
+        // Конструктор для инициализации полей
+        public Notebook(string model, string manufacturer, decimal price)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Price = price;
+        }
+
+        // Метод для вывода содержимого полей на экран
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Модель: {Model}");
+            Console.WriteLine($"Производитель: {Manufacturer}");
+            Console.WriteLine($"Цена: {Price:C}");
+        }
+
+    }
+}
---
+++ Задача 22/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание объекта структуры Notebook с использованием конструктора
+            Notebook notebook1 = new Notebook("MacBook Air", "Apple", 999.99m);
+
+            // Вывод информации о ноутбуке
+            Console.WriteLine("Информация о ноутбуке:");
+            notebook1.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
