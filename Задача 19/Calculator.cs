2233fb75fdcb2ed9bd3f1b5dbd0f6aa8538e4308
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-19 13:20:31 +0300

    Add files via upload

---
+++ Задача 19/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_19
+{
+    static class Calculator
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Mul(int x, int y)
+        {
+            return x * y;
+        }
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
