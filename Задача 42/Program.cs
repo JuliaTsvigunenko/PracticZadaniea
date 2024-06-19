6aeafe6232d513b38c9324ae3b0c413597588154
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-19 15:00:31 +0300

    Add files via upload

---
+++ Задача 42/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример использования класса Triangle с разными типами данных
+            Triangle<int, int, int> intTriangle = new Triangle<int, int, int>(3, 4, 5);
+            intTriangle.PrintInfo();
+            Console.WriteLine($"Площадь треугольника: {intTriangle.CalculateArea()}");
+
+            Triangle<uint, uint, uint> uintTriangle = new Triangle<uint, uint, uint>(6, 8, 10);
+            uintTriangle.PrintInfo();
+            Console.WriteLine($"Площадь треугольника: {uintTriangle.CalculateArea()}");
+
+            Triangle<double, double, double> doubleTriangle = new Triangle<double, double, double>(2.5, 3.5, 4.5);
+            doubleTriangle.PrintInfo();
+            Console.WriteLine($"Площадь треугольника: {doubleTriangle.CalculateArea()}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Задача 42/Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_42
+{
+
+    // Обобщенный класс Triangle
+    public class Triangle<T, P, R>
+    {
+        // Поля треугольника
+        private T sideA;
+        private P sideB;
+        private R sideC;
+
+        // Конструктор с параметрами для инициализации сторон треугольника
+        public Triangle(T a, P b, R c)
+        {
+            // Проверка условия существования треугольника
+            if ((dynamic)Convert.ToDouble(a) + Convert.ToDouble(b) > Convert.ToDouble(c) &&
+                (dynamic)Convert.ToDouble(a) + Convert.ToDouble(c) > Convert.ToDouble(b) &&
+                (dynamic)Convert.ToDouble(b) + Convert.ToDouble(c) > Convert.ToDouble(a))
+            {
+                sideA = a;
+                sideB = b;
+                sideC = c;
+            }
+            else
+            {
+                Console.WriteLine("Треугольник с такими сторонами не существует");
+            }
+        }
+
+        // Свойства для доступа к полям треугольника
+        public T A { get => sideA; set => sideA = value; }
+        public P B { get => sideB; set => sideB = value; }
+        public R C { get => sideC; set => sideC = value; }
+
+        // Метод для вывода информации о треугольнике
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Треугольник со сторонами: {sideA}, {sideB}, {sideC}");
+        }
+
+        // Метод для вычисления площади треугольника
+        public double CalculateArea()
+        {
+            // Приведение к double для вычисления площади
+            double a = Convert.ToDouble(sideA);
+            double b = Convert.ToDouble(sideB);
+            double c = Convert.ToDouble(sideC);
+
+            // Полупериметр треугольника
+            double s = (a + b + c) / 2;
+
+            // Формула Герона для вычисления площади треугольника
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+            return area;
+        }
+    }
+}
+
