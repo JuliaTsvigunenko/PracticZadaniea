a0baf5b67e17ed082776683a9f2c9ba10d058482
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-19 14:35:27 +0300

    Add files via upload

---
+++ Задача 34/IInterface1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_34
+{
+    interface IInterface1
+    {
+        void Method1();
+    }
+
+
+    interface IInterface2 : IInterface1
+    {
+        void Method2();
+    }
+
+
+    interface IInterface3 : IInterface2
+    {
+        void Method3();
+    }
+}
