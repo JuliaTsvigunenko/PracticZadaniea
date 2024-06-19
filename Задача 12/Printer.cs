8f5a3e15341fb1fcb0d725735352b8616a32b9ab
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2024-06-19 12:57:47 +0300

    Add files via upload

---
+++ Задача 12/Printer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_12
+{
+    class Printer
+    {
+        // Определение метода Print в базовом классе Printer.
+        public virtual void Print(string value)
+        {
+            // Метод выводит переданное значение (строку) на экран.
+            // Используется метод Console.WriteLine, который выводит строку и переходит на новую строку.
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ Задача 12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Запрашиваем у пользователя ввод строки.
+            Console.Write("Введите строку: ");
+            // Считываем введенную пользователем строку и сохраняем ее в переменную `value`.
+            string value = Console.ReadLine();
+            // Создаем экземпляр базового класса Printer, который будет выводить текст без изменения цвета.
+            Printer defaultPrinter = new Printer();
+            // Создаем экземпляр производного класса RedPrinter, который будет выводить текст красным цветом.
+            Printer redPrinter = new RedPrinter();
+            // Вызываем метод Print базового класса Printer, который выводит текст без изменения цвета.
+            defaultPrinter.Print(value);
+            // Вызываем метод Print производного класса RedPrinter, который выводит текст красным цветом.
+            redPrinter.Print(value);
+            // Ожидаем, пока пользователь нажмет Enter, чтобы завершить выполнение программы.
+            Console.ReadLine();
+
+        }
+    }
+}
